8592232feed81ee6def1fa56503a55b7edbd578e
liyabo <liyabo@hetao101.com>
2020-12-10 15:34:58 +0800

    text

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import { DefaultFooter } from '@ant-design/pro-layout';
 
-export default () => <DefaultFooter copyright="2020 核桃编程教育事业部技术团队出品" links={[]} />;
+export default () => <DefaultFooter copyright="2020 教育事业部技术团队出品" links={[]} />;
